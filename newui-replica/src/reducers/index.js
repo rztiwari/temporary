69378bdafe5f6a3ca67369cd74e50e5d045b085c
rztiwari <rztiwari@gmail.com>
2017-07-20 23:26:04 +0100

    Removed

--- newui-replica/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import paymentDetails from './paymentDetailsReducer';
-
-
-var rootReducer = combineReducers({
-  paymentDetails
-});
-
-export default rootReducer;
